a2c5fc02bad59d9cb4ed5bf6088e2a2f8d14aac1
ali iranpour <iranpour181@gmail.com>
2024-07-05 23:52:33 +0330

    add call us part

--- src/pages/call us/Call_us.jsx
+++ src/pages/call us/Call_us.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
-import ChatWidget from '../../components/Chatwidget/Chatpart'
 import Cooperation from '../../components/Cooperation/Cooperation'
 import { Container } from 'react-bootstrap'
 
 const Call_us = () => {
   return (
     <Container>
-
+      <div className=' border border-5 border-danger p-5 rounded-4'>
+        <div className=' my-3' >
+          <h3>نشانی: </h3>
+          <p className=' mx-5'>اصفهان، رضوانشهر، شهرک تخصصی سنگ، خیابان سوم مرکزی</p>
+        </div>
+        <div>
+          <h3>تلفن های تماس :</h3>
+          <div className=' mx-5 my-3'>
+            <div className=' my-3'>
+                <span>مدیریت: </span> <a href="tel:09133109305" className=" text-decoration-none text-black"> 09133109305 </a>
+            </div>
+            <div className=' my-3'>
+              <span>بازرگانی: </span>
+              <a href="tel:09133109815" className=" text-decoration-none text-black"> 09133109815 </a>
+            </div>
+            <div className=' my-3'>
+              <span>تلفکس: </span>
+              <a href="tel:03142524114" className=" text-decoration-none text-black"> 03142524114 </a>
+            </div>
+          </div>
+        </div>
       <Cooperation />
+      </div>
     </Container>
   )
 }
